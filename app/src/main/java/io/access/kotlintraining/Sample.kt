6eb90da7d0287e8bf7848ef1eb38d31235640491
aYukiMizuno <yuki.mizuno@access-company.com>
2019-04-18 17:43:20 +0900

    04/18 Practice

--- app/src/main/java/io/access/kotlintraining/Sample.kt
+++ app/src/main/java/io/access/kotlintraining/Sample.kt
@@ -1,11 +1,158 @@
 package io.access.kotlintraining
 
+//import android.widget.TabHost
+//import android.support.annotation.NonNull
+
+
 fun welcome() {
     println("welcome to ACCESS!")
 }
 
-// TODO: declare hello function
 
-fun hello(){
+//
+
+
+fun hello() {
     println("Hello World!")
+}
+
+
+//
+
+
+fun fizzbuzz() {
+    for (i in 1..100) {
+        println(
+                when {
+                    i % 15 == 0 -> "FizzBuzz"
+                    i % 5 == 0 -> "Buzz"
+                    i % 3 == 0 -> "Fizz"
+                    else -> i.toString()
+                }
+        )
+    }
+}
+
+
+//
+
+
+fun isLeapYear(year: Int): Boolean =
+        when{
+            (year % 400 == 0) -> true
+            (year % 100 == 0) -> false
+            (year % 4 == 0) -> true
+            else -> false
+        }
+
+
+//
+
+
+fun powerInt(a: Int, n: Int): Long {
+
+    if (a < 0 || n < 0)
+        error("inputs must be positive. return 0")
+
+    var y = 1L
+    for (i in 1..n)
+        y *= a
+
+    return y
+}
+
+
+//
+
+
+fun power(a: Double, n: Int): Double {
+    if (a < 0 || n < 0)
+        error("inputs must be positive. return 0")
+
+    // 例外
+    if (n == 0)
+        return 1.0
+    if (n == 1)
+        return a
+
+    var p = a // 掛ける数
+    var m = n // 掛ける回数
+
+    var y = 1.0 // 結果　
+    while (0 < m) {
+        if (m % 2 == 1) y *= p
+        p *= p
+        m /= 2
+    }
+
+    return y
+}
+
+
+//
+
+
+fun Int.isOdd() = this.and(1) == 1
+fun Int.isEven() = this.and(1) == 0
+
+
+//
+
+
+class Dice constructor(private val n: Int) {
+    private var life = 100
+    fun roll(): Int {
+        if ((--life) <= 0) throw Exception("I was broken")
+        return 1 + Math.floor(Math.random() * n.toDouble()).toInt()
+    }
+}
+
+
+//
+
+
+class MyCustomClass {
+    var counter: Int = 0
+    var propertyWithCounter: Int = 0
+        set(v) {
+            ++counter
+            field = v
+        }
+}
+
+
+//
+
+
+class NabeAtsu {
+    var cnt = 0
+
+    private fun has3(n: Int): Boolean =
+            when {
+                n %10 == 3 -> true
+                n <= 0     -> false
+                else       -> has3(n/10)
+            }
+
+    fun next(): String =
+            if (++cnt % 3 == 0 || has3(cnt)) "Aho"
+            else cnt.toString()
+}
+
+
+//
+
+
+// no tests
+fun sendMessageToClient(client: Client?, message: String?, mailer: Mailer) {
+    if (message != null)
+        client?.personalInfo?.email?.let { mailer.sendMessage(it, message) }
+}
+
+
+// コンパイル通すための諸々
+class Client (val personalInfo: PersonalInfo?)
+class PersonalInfo (val email: String?)
+interface Mailer {
+    fun sendMessage(email: String, message: String)
 }
--- app/src/test/java/io/access/kotlintraining/SampleTest.kt
+++ app/src/test/java/io/access/kotlintraining/SampleTest.kt
@@ -2,11 +2,13 @@ package io.access.kotlintraining
 
 import org.junit.Test
 import kotlin.test.assertEquals
+import kotlin.test.assertFails
 
 class SampleTest {
     @Test
     fun testWelcome() {
-        welcome()
+        // welcome()
+        // fizzbuzz()
 
         // Example of test
         val expected = 1
@@ -17,6 +19,94 @@ class SampleTest {
     @Test
     fun testHello() {
         // TODO: implement to call hello function
-        hello()
+        // hello()
+    }
+
+    @Test
+    fun testIsLeapYear() {
+        val no = listOf(1700, 1800, 1900, 2100, 2200, 2300, 2500, 2600)
+        val yes = listOf(1600, 2400, 2000)
+        for (y in yes) {
+            assertEquals(true, isLeapYear(y), "fail: $y")
+        }
+        for (y in no) {
+            assertEquals(false, isLeapYear(y), "fail: $y")
+        }
+    }
+
+    @Test
+    fun testPowerInt() {
+        assertEquals(1, powerInt(0, 0))
+        assertEquals(1, powerInt(1, 0))
+        assertEquals(0, powerInt(0, 1))
+        assertEquals(1, powerInt(1, 1))
+
+        assertEquals(256, powerInt(2, 8))
+    }
+
+    @Test
+    fun testPower() {
+        assertEquals(1.0, power(0.0, 0))
+        assertEquals(1.0, power(1.0, 0))
+        assertEquals(0.0, power(0.0, 1))
+        assertEquals(1.0, power(1.0, 1))
+
+        assertEquals(256.0, power(2.0, 8))
+        assertEquals(128.0, power(2.0, 7))
+        assertEquals(32.0, power(2.0, 5))
+
+        assertEquals(2216_83782.0, Math.floor(power(1.5, 19) * 100000))
+    }
+
+    @Test
+    fun testEvenOdd(){
+        assert(1.isOdd())
+        assert(5.isOdd())
+        assert((-5).isOdd())
+
+        assert(!1.isEven())
+        assert(!5.isEven())
+        assert(!(-5).isEven())
+
+        assert(2.isEven())
+        assert(4.isEven())
+        assert((-4).isEven())
+        assert(0.isEven())
+
+        assert(!2.isOdd())
+        assert(!4.isOdd())
+        assert(!(-4).isOdd())
+        assert(!0.isOdd())
+    }
+
+    @Test
+    fun testDice() {
+        var dice = Dice(6)
+        for (i in 1..99) {
+            val x = dice.roll()
+            assert((1..6).contains(x))
+        }
+        assertFails { dice.roll() }
+    }
+
+    @Test
+    fun testMyCustomClass() {
+        val mcc = MyCustomClass()
+        mcc.propertyWithCounter = 0
+        mcc.propertyWithCounter = 0
+        assertEquals(2, mcc.counter)
+        mcc.propertyWithCounter = 0
+        assertEquals(3, mcc.counter)
+
+    }
+
+    @Test
+    fun testNabe() {
+        var na = NabeAtsu()
+        val expected = listOf("1", "2", "Aho", "4", "5", "Aho", "7", "8", "Aho", "10", "11", "Aho", "Aho")
+
+        for (e in expected) {
+            assertEquals(e, na.next())
+        }
     }
 }
